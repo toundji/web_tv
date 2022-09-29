c2717a1fd14f048310899e069d0474e8af3615c1
toundji <olatoundjiagbahoungba@gmail.com>
2022-09-29 12:07:16 +0100

    ......

--- lib/screens/home.screen.dart
+++ lib/screens/home.screen.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:web_tv/screens/drawer.dart';
-=======
 import 'package:web_tv/utils/constants.util.dart';
-
->>>>>>> 1a1b61d2a0b2e1620a6b22b468d94b801b4b87cc
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -16,15 +11,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return Scaffold(
-      appBar: AppBar(),
-      drawer: AppDrawer(),
-      body: ListView(),
-=======
     return Container(
       decoration: appGradient,
->>>>>>> 1a1b61d2a0b2e1620a6b22b468d94b801b4b87cc
     );
   }
 }
